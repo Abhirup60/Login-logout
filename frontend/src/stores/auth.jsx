5916320e4ac8406153d3c65017e3021741f2d46e
Abhirup60 <abhirupgit36@gmail.com>
2025-02-14 09:12:20 +0530

    update

--- frontend/src/stores/auth.jsx
+++ frontend/src/stores/auth.jsx
@@ -43,7 +43,7 @@ export const AuthProvider = ({ children }) => {
 
   useEffect(() => {
     authenticateUser();
-  }, [authenticateUser]);
+  }, []);
 
 // -------------------------- user authentication --------------------------//
 
